813a9745f0682fe10b672d68f13c3c8cd802301d
Татьяна <tanya.karpova.ry@gmail.com>
2024-08-21 12:20:00 +0300

    Добавляет слайдер hero

---
+++ source/js/hero-slide.js
@@ -0,0 +1,29 @@
+import Swiper from 'swiper';
+import 'swiper/css';
+import {Pagination} from 'swiper/modules';
+
+new Swiper('.swiper', {
+  modules: [Pagination],
+  loop: true,
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  breakpoints: {
+    320: {
+      allowTouchMove: true,
+      pagination: false,
+    },
+
+    768: {
+      allowTouchMove: true,
+      pagination: false,
+    },
+
+    1440: {
+      allowTouchMove: false,
+    }
+  }
+});
